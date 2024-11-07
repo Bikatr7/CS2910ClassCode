6aecfe0aaf6b47c070a4404502f78bcf4f0682c9
Bikatr7 <Bikatr7@proton.me>
2024-11-06 21:01:11 -0700

    And the rest of the relevant stores part

--- mdn-svelte-tutorial/06-stores/src/App.svelte
+++ mdn-svelte-tutorial/06-stores/src/App.svelte
@@ -1,14 +1,15 @@
-<!-- App.svelte -->
 <script>
+  import Todos from "./components/Todos.svelte";
   import Alert from "./components/Alert.svelte";
-  import Todos from './components/Todos.svelte'
 
-  let todos = [
-    { id: 1, name: 'Create a Svelte starter app', completed: true },
-    { id: 2, name: 'Create your first component', completed: true },
-    { id: 3, name: 'Complete the rest of the tutorial', completed: false }
-  ]
+  import { todos } from "./stores.js";
+
+  $todos = [
+    { id: 1, name: "Create a Svelte starter app", completed: true },
+    { id: 2, name: "Create your first component", completed: true },
+    { id: 3, name: "Complete the rest of the tutorial", completed: false }
+  ];
 </script>
 
 <Alert />
-<Todos {todos} />
+<Todos bind:todos={$todos} />
