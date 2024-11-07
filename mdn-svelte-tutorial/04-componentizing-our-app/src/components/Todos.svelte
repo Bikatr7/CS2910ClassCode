08677f4f10c907f916c5fd0ad52e84c1f1165cde
Bikatr7 <Bikatr7@proton.me>
2024-11-06 18:08:56 -0700

    Breaking the app into components -> Extracting our filter component -> Sharing data between components: passing a handler as a prop -> Easier two-way data binding with the bind directive

--- mdn-svelte-tutorial/04-componentizing-our-app/src/components/Todos.svelte
+++ mdn-svelte-tutorial/04-componentizing-our-app/src/components/Todos.svelte
@@ -1,5 +1,6 @@
 <!-- components/Todos.svelte -->
 <script>
+  import FilterButton from './FilterButton.svelte'
   export let todos = []
   
   let newTodoName = ''
@@ -42,23 +43,8 @@
   </form>
 
   <!-- Filter -->
-  <div class="filters btn-group stack-exception">
-    <button class="btn toggle-btn" class:btn__primary={filter === 'all'} aria-pressed={filter === 'all'} on:click={()=> filter = 'all'} >
-      <span class="visually-hidden">Show</span>
-      <span>All</span>
-      <span class="visually-hidden">tasks</span>
-    </button>
-    <button class="btn toggle-btn" class:btn__primary={filter === 'active'} aria-pressed={filter === 'active'} on:click={()=> filter = 'active'} >
-      <span class="visually-hidden">Show</span>
-      <span>Active</span>
-      <span class="visually-hidden">tasks</span>
-    </button>
-    <button class="btn toggle-btn" class:btn__primary={filter === 'completed'} aria-pressed={filter === 'completed'} on:click={()=> filter = 'completed'} >
-      <span class="visually-hidden">Show</span>    
-      <span>Completed</span>
-      <span class="visually-hidden">tasks</span>
-    </button>
-  </div>
+  <FilterButton bind:filter={filter} />
+
 
   <!-- TodosStatus -->
   <h2 id="list-heading">{completedTodos} out of {totalTodos} items completed</h2>
